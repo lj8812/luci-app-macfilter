3ec4360ae65b3045534b092f99b9ed96b93c5188
lj8812 <74142076+lj8812@users.noreply.github.com>
2025-03-04 18:02:50 +0800

    Update macfilter.lua

--- luasrc/controller/macfilter.lua
+++ luasrc/controller/macfilter.lua
@@ -2,5 +2,5 @@ module("luci.controller.macfilter", package.seeall)
 
 function index()
     -- 在"服务"菜单下添加条目
-    entry({"admin", "services", "macfilter"}, cbi("macfilter"), _("MAC Filter"), 30).dependent = false
+    entry({"admin", "services", "macfilter"}, cbi("macfilter"), _("MAC过滤"), 30).dependent = false
 end
