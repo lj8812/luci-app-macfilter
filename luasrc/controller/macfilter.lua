ba6e5d7a81793ab2413338ad680865a426ce2639
lj8812 <74142076+lj8812@users.noreply.github.com>
2025-03-04 09:12:21 +0800

    Create macfilter.lua

---
+++ luasrc/controller/macfilter.lua
@@ -0,0 +1,6 @@
+module("luci.controller.macfilter", package.seeall)
+
+function index()
+    -- 在"服务"菜单下添加条目
+    entry({"admin", "services", "macfilter"}, cbi("macfilter"), _("MAC Filter"), 30).dependent = false
+end
