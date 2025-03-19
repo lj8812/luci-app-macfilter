4071ff2626d0235536241e764b1dcdb41909b61f
lj8812 <74142076+lj8812@users.noreply.github.com>
2025-03-19 18:02:27 +0800

    Delete luasrc directory

--- luasrc/controller/macfilter.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.macfilter", package.seeall)
-
-function index()
-    -- 在"服务"菜单下添加条目
-    entry({"admin", "services", "macfilter"}, cbi("macfilter"), _("MAC过滤"), 30).dependent = false
-end
